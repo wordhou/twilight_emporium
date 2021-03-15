f81bf03ee888c4732f1249a563f9f205291a82cb
Ed <houedward@gmail.com>
2021-03-15 11:04:00 -0700

    setting up to work on heroku

--- src/app/db/index.ts
+++ src/app/db/index.ts
@@ -1,6 +1,16 @@
 import { Pool, QueryResult } from "pg";
 
-const pool = new Pool();
+let pool: Pool;
+if (process.env["LOCAL"] === "local") {
+  pool = new Pool();
+} else {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+}
 
 const query = (text: string, params?: any[]): Promise<QueryResult<any>> =>
   pool.query(text, params);
